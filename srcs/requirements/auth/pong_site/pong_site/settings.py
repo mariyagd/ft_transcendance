e1082b4c9fa4abbeea90ee53d46756d2667088f6
mariyagd <109855801+mariyagd@users.noreply.github.com>
2024-09-18 11:00:25 +0200

    tokens life

--- srcs/requirements/auth/pong_site/pong_site/settings.py
+++ srcs/requirements/auth/pong_site/pong_site/settings.py
@@ -154,8 +154,8 @@ AUTHENTICATION_BACKENDS = [
 
 # ----------------------------------------------------------------------------------------------------------------------
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
